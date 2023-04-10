aef99cdcbbffd4dbc8988118d419753b869e9790
Moyowaaaa <moyowa7@icloud.com>
2023-04-10 11:26:01 +0100

    some code 🚀

---
+++ src/ts/main.ts
@@ -0,0 +1,18 @@
+
+
+console.log('hola')
+
+
+
+
+const testArray:string[] = ['hola','yesy','bruhh','gg']
+const imageContainer = document.querySelector('#test')
+
+testArray.forEach((test:string) => {
+imageContainer.innerHTML = test
+
+})
+
+
+const numbers = [1, 2, 3, 4, 5];
+
