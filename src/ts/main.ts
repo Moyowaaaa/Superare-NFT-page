958ee746304e441a35e219c31f0396e30fe0bda1
Moyowaaaa <moyowa7@icloud.com>
2023-04-21 22:50:35 +0100

    🚀

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,17 +1,41 @@
 
-console.log('hola')
+
+import LocomotiveScroll from "locomotive-scroll";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 
+gsap.registerPlugin(ScrollTrigger);
 
-const testArray:string[] = ['hola','yesy','bruhh','gg']
-const imageContainer = document.querySelector('#test')
-
-testArray.forEach((test:string) => {
-imageContainer.innerHTML = test
-
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
 })
 
+scroller.on('scroll', ScrollTrigger.update)
 
-const numbers = [1, 2, 3, 4, 5];
+ScrollTrigger.scrollerProxy(
+    '.container', {
+        scrollTop(value) {
+            return arguments.length ?
+            scroller.scrollTo(value, 0, 0) :
+            scroller.scroll.instance.scroll.y
+        },
+        getBoundingClientRect() {
+            return {
+                left: 0, top: 0, 
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        }
+    }
+)
+
+
+ScrollTrigger.addEventListener('refresh', () => scroller.update())
+
+
+ScrollTrigger.refresh()
+
 
