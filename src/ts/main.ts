abd04fd9348136f022844ae69f06101257409b81
Moyowaaaa <moyowa7@icloud.com>
2023-04-28 10:53:27 +0100

    🚀

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,41 +1,41 @@
 
 
-import LocomotiveScroll from "locomotive-scroll";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import LocomotiveScroll from "locomotive-scroll";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-})
+// const scroller = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true
+// })
 
-scroller.on('scroll', ScrollTrigger.update)
+// scroller.on('scroll', ScrollTrigger.update)
 
-ScrollTrigger.scrollerProxy(
-    '.container', {
-        scrollTop(value) {
-            return arguments.length ?
-            scroller.scrollTo(value, 0, 0) :
-            scroller.scroll.instance.scroll.y
-        },
-        getBoundingClientRect() {
-            return {
-                left: 0, top: 0, 
-                width: window.innerWidth,
-                height: window.innerHeight
-            }
-        }
-    }
-)
-
-
-ScrollTrigger.addEventListener('refresh', () => scroller.update())
+// ScrollTrigger.scrollerProxy(
+//     '.container', {
+//         scrollTop(value) {
+//             return arguments.length ?
+//             scroller.scrollTo(value, 0, 0) :
+//             scroller.scroll.instance.scroll.y
+//         },
+//         getBoundingClientRect() {
+//             return {
+//                 left: 0, top: 0, 
+//                 width: window.innerWidth,
+//                 height: window.innerHeight
+//             }
+//         }
+//     }
+// )
 
 
-ScrollTrigger.refresh()
+// ScrollTrigger.addEventListener('refresh', () => scroller.update())
+
+
+// ScrollTrigger.refresh()
 
 
