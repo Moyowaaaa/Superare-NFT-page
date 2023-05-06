074243e33f8b8251b3e7782e0df2082ac860fe93
Moyowaaaa <moyowa7@icloud.com>
2023-05-07 00:00:19 +0100

    refractoring

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -67,349 +67,349 @@ requestAnimationFrame(scrollFn);
 
 //Preloader
 
-window.addEventListener("load", () => {
-  document.body.style.position = "fixed";
-  document.body.style.overflow = "hidden";
+// window.addEventListener("load", () => {
+//   document.body.style.position = "fixed";
+//   document.body.style.overflow = "hidden";
 
-  const promises: any = [];
-  const loadImages = [showcaseImageOne, showcaseImageTwo, showcaseImageThree];
+//   const promises: any = [];
+//   const loadImages = [showcaseImageOne, showcaseImageTwo, showcaseImageThree];
 
-//   new Promise((resolve, reject) => {
-//     const polySansBoldFont = new FontFace(
-//       "polysansBold",
-//       `url(${polySansBold})`
+// //   new Promise((resolve, reject) => {
+// //     const polySansBoldFont = new FontFace(
+// //       "polysansBold",
+// //       `url(${polySansBold})`
+// //     );
+// //     const polysansMediumFont = new FontFace(
+// //       "polysansMedium",
+// //       `url(${polysansMedium})`
+// //     );
+// //     const circularBoldFont = new FontFace(
+// //       "circeularBold",
+// //       `url(${circularBold})`
+// //     );
+// //     const circularFont = new FontFace("circular", `url(${circular})`);
+// //     const polysansFont = new FontFace("polysans", `url(${polysans})`);
+// //     const robotoLightFont = new FontFace("robotoLight", `url(${robotoLight})`);
+// //     // const robotoFont = new FontFace('roboto', `url(${roboto})`)
+
+// //     const allFonts = [
+// //       polySansBoldFont,
+// //       polysansFont,
+// //       polysansMediumFont,
+// //       circularBoldFont,
+// //       circularFont,
+// //       robotoLightFont,
+// //     ];
+
+// //     allFonts.forEach((fonts) => {
+// //       fonts.load().then(resolve, reject);
+// //     });
+// //   });
+//   loadImages.forEach((asset) => {
+//     const img = new Image();
+//     img.src = asset;
+//     promises.push(
+//       new Promise((resolve, reject) => {
+//         img.onload = resolve;
+//         img.onerror = reject;
+//       })
 //     );
-//     const polysansMediumFont = new FontFace(
-//       "polysansMedium",
-//       `url(${polysansMedium})`
-//     );
-//     const circularBoldFont = new FontFace(
-//       "circeularBold",
-//       `url(${circularBold})`
-//     );
-//     const circularFont = new FontFace("circular", `url(${circular})`);
-//     const polysansFont = new FontFace("polysans", `url(${polysans})`);
-//     const robotoLightFont = new FontFace("robotoLight", `url(${robotoLight})`);
-//     // const robotoFont = new FontFace('roboto', `url(${roboto})`)
+//   });
 
-//     const allFonts = [
-//       polySansBoldFont,
-//       polysansFont,
-//       polysansMediumFont,
-//       circularBoldFont,
-//       circularFont,
-//       robotoLightFont,
-//     ];
+//   Promise.all(promises).then(() => {
+//     loaderTl.play();
+//   });
 
-//     allFonts.forEach((fonts) => {
-//       fonts.load().then(resolve, reject);
+//   const loaderTl = gsap.timeline({ paused: true });
+
+//   const counter = document.querySelector(".count");
+//   let loaded = 0;
+
+//   loaderTl.fromTo(
+//     "#loadImage1",
+//     {
+//       opacity: 0,
+//       y: 100,
+//       ease: "power3.inOut",
+//     },
+//     {
+//       duration: 0.4,
+//       display: "flex",
+//       ease: "power.inOut",
+//       opacity: 1,
+//       y: 0,
+//     }
+//   );
+
+//   if (viewport > mobile) {
+//     loaderTl
+//       .fromTo(
+//         "#loadImage2",
+//         {
+//           opacity: 0,
+//           y: 100,
+//         },
+//         {
+//           y: 0,
+//           opacity: 1,
+//           ease: "power3.inOut",
+//           duration: 0.5,
+//           display: "flex",
+//         }
+//       )
+//       .fromTo(
+//         "#loadImage3",
+//         {
+//           opacity: 0,
+//           y: 100,
+//         },
+//         {
+//           y: 0,
+//           opacity: 1,
+//           ease: "power3.inOut",
+//           duration: 0.6,
+//           display: "flex",
+//         }
+//       )
+//       .fromTo(
+//         "#loadImage4",
+//         {
+//           opacity: 0,
+//           y: 100,
+//         },
+//         {
+//           y: 0,
+//           opacity: 1,
+//           ease: "power3.inOut",
+//           duration: 0.7,
+//           display: "flex",
+//         }
+//       )
+
+//       .to("#loadImage1", {
+//         opacity: 0,
+//         duration: 0.8,
+//       })
+//       .to("#loadImage2", {
+//         opacity: 0,
+//         duration: 0.6,
+//       })
+//       .to("#loadImage3", {
+//         opacity: 0,
+//         duration: 0.4,
+//       })
+//       .to("#loadImage4", {
+//         y: "-5rem",
+//         opacity: 0,
+//         duration: 0.2,
+//       });
+//   }
+
+//   //loader count
+
+//   const interval = setInterval(() => {
+//     const percent = Math.round((loaded / 99) * 100);
+//     counter.innerHTML = percent.toString();
+//     loaded++;
+
+//     //Remove loader
+
+//     if (loaded === 100) {
+//       clearInterval(interval);
+//       loaderTl
+//       .to(".preloader", {
+//         y: "-100%",
+//         ease: "power3.inOut",
+//         duration: 0.5,
+//         display: "none",
+//         zIndex: 0,
+//       })
+//       .fromTo(
+//         ".container",
+//         {
+//           display: "none",
+//           opacity: 0,
+//         },
+//         {
+//           display: "block",
+//           opacity: 1,
+//           ease: "none",
+//           duration: 0.01,
+//           onComplete: () => {
+//             animations();
+//           },
+//         }
+//       );
+//     }
+//   }, 35);
+// });
+
+// const animations = () => {
+//   document.body.style.position = "";
+//   document.body.style.overflow = "";
+
+//   const navTl = gsap.timeline();
+
+//   navTl.fromTo(
+//     ".navbar-logo",
+//     { opacity: 0 },
+//     {
+//       y: 0,
+//       opacity: 1,
+//       duration: 1.5,
+//       clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
+//     },
+//     "<0.1"
+//   );
+
+//   navTl.from(
+//     ".nav-regular h3",
+//     {
+//       x: 10,
+//       opacity: 0,
+//       ease: "power3.inOut",
+//       stagger: 0.04,
+//     },
+//     "<0.5"
+//   );
+
+//   navTl.from(".navbar__button", { opacity: 0 });
+
+//   const titleSectionTl = gsap.timeline();
+
+//   titleSectionHead.forEach((title) => {
+//     titleSectionTl.from(title.querySelectorAll(".word"), {
+//       opacity: 0,
+//       duration: 0.8,
+//       y: 20,
+//       stagger: 0.2,
+//       delay: 0.9,
 //     });
 //   });
-  loadImages.forEach((asset) => {
-    const img = new Image();
-    img.src = asset;
-    promises.push(
-      new Promise((resolve, reject) => {
-        img.onload = resolve;
-        img.onerror = reject;
-      })
-    );
-  });
 
-  Promise.all(promises).then(() => {
-    loaderTl.play();
-  });
+//   titleSectionTl
+//     .from(
+//       ".titleSection__main-container--image-container",
+//       {
+//         opacity: 0,
+//         scale: 1.2,
+//         ease: "Power3.inOut",
+//         duration: 0.7,
+//       },
+//       "<0.1"
+//     )
+//     .from(
+//       [".titleSection-text", ".titleSection-button"],
+//       {
+//         opacity: 0,
+//         y: 20,
+//         ease: "power3.inOut",
+//       },
+//       "<0.1"
+//     )
+//     .fromTo(
+//       ".titleSection-clients",
+//       { opacity: 0 },
+//       {
+//         y: 0,
+//         opacity: 1,
+//         duration: 0.7,
+//         clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
+//       },
+//       "<0.3"
+//     );
 
-  const loaderTl = gsap.timeline({ paused: true });
+//   gsap.fromTo(
+//     ".hero-section__hero-container--image img",
+//     {
+//       opacity: 0,
+//     },
+//     {
+//       opacity: 1,
+//       ease: "Power3.inOut",
+//       duration: 1.2,
+//       scrollTrigger: {
+//         trigger: ".hero-section__hero-container--image img",
+//         start: "top bottom-=20%",
+//         end: "center top+=20%",
+//         scrub: true,
+//       },
+//     }
+//   );
 
-  const counter = document.querySelector(".count");
-  let loaded = 0;
+//   heroSectionTitle.forEach((title) => {
+//     gsap.from(title.querySelectorAll(".word"), {
+//       opacity: 0,
+//       y: 20,
+//       stagger: 0.05,
+//       scrollTrigger: {
+//         trigger: title,
+//         start: "top bottom-=20%",
+//         end: "center top+=20%",
+//         scrub: true,
+//       },
+//     });
+//   });
 
-  loaderTl.fromTo(
-    "#loadImage1",
-    {
-      opacity: 0,
-      y: 100,
-      ease: "power3.inOut",
-    },
-    {
-      duration: 0.4,
-      display: "flex",
-      ease: "power.inOut",
-      opacity: 1,
-      y: 0,
-    }
-  );
+//   heroSectionText.forEach((text) => {
+//     gsap.fromTo(
+//       text.querySelectorAll(".word"),
+//       {
+//         "will-change": "opacity",
+//         opacity: 0.1,
+//       },
+//       {
+//         ease: "none",
+//         opacity: 1,
+//         stagger: 0.05,
+//         scrollTrigger: {
+//           trigger: text,
+//           start: "top bottom-=20%",
+//           end: "center top+=20%",
+//           scrub: true,
+//         },
+//       }
+//     );
+//   });
 
-  if (viewport > mobile) {
-    loaderTl
-      .fromTo(
-        "#loadImage2",
-        {
-          opacity: 0,
-          y: 100,
-        },
-        {
-          y: 0,
-          opacity: 1,
-          ease: "power3.inOut",
-          duration: 0.5,
-          display: "flex",
-        }
-      )
-      .fromTo(
-        "#loadImage3",
-        {
-          opacity: 0,
-          y: 100,
-        },
-        {
-          y: 0,
-          opacity: 1,
-          ease: "power3.inOut",
-          duration: 0.6,
-          display: "flex",
-        }
-      )
-      .fromTo(
-        "#loadImage4",
-        {
-          opacity: 0,
-          y: 100,
-        },
-        {
-          y: 0,
-          opacity: 1,
-          ease: "power3.inOut",
-          duration: 0.7,
-          display: "flex",
-        }
-      )
+//   if (viewport > mobile) {
+//     firstShowcaseTitle.forEach((text) => {
+//       gsap.from(text.querySelectorAll(".word"), {
+//         opacity: 0,
+//         y: 20,
+//         stagger: 0.05,
+//         scrollTrigger: {
+//           trigger: text,
+//           start: "top bottom-=20%",
+//           end: "center top+=20%",
+//           scrub: true,
+//         },
+//       });
+//     });
 
-      .to("#loadImage1", {
-        opacity: 0,
-        duration: 0.8,
-      })
-      .to("#loadImage2", {
-        opacity: 0,
-        duration: 0.6,
-      })
-      .to("#loadImage3", {
-        opacity: 0,
-        duration: 0.4,
-      })
-      .to("#loadImage4", {
-        y: "-5rem",
-        opacity: 0,
-        duration: 0.2,
-      });
-  }
+//     showcaseImages.forEach((image) => {
+//       gsap.to(image, {
+//         scale: 1.2,
 
-  //loader count
+//         scrollTrigger: {
+//           trigger: image,
+//           start: "top bottom-=20%",
+//           end: "center top+=20%",
+//           scrub: true,
+//         },
+//       });
+//     });
 
-  const interval = setInterval(() => {
-    const percent = Math.round((loaded / 99) * 100);
-    counter.innerHTML = percent.toString();
-    loaded++;
-
-    //Remove loader
-
-    if (loaded === 100) {
-      clearInterval(interval);
-      loaderTl
-      .to(".preloader", {
-        y: "-100%",
-        ease: "power3.inOut",
-        duration: 0.5,
-        display: "none",
-        zIndex: 0,
-      })
-      .fromTo(
-        ".container",
-        {
-          display: "none",
-          opacity: 0,
-        },
-        {
-          display: "block",
-          opacity: 1,
-          ease: "none",
-          duration: 0.01,
-          onComplete: () => {
-            animations();
-          },
-        }
-      );
-    }
-  }, 35);
-});
-
-const animations = () => {
-  document.body.style.position = "";
-  document.body.style.overflow = "";
-
-  const navTl = gsap.timeline();
-
-  navTl.fromTo(
-    ".navbar-logo",
-    { opacity: 0 },
-    {
-      y: 0,
-      opacity: 1,
-      duration: 1.5,
-      clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
-    },
-    "<0.1"
-  );
-
-  navTl.from(
-    ".nav-regular h3",
-    {
-      x: 10,
-      opacity: 0,
-      ease: "power3.inOut",
-      stagger: 0.04,
-    },
-    "<0.5"
-  );
-
-  navTl.from(".navbar__button", { opacity: 0 });
-
-  const titleSectionTl = gsap.timeline();
-
-  titleSectionHead.forEach((title) => {
-    titleSectionTl.from(title.querySelectorAll(".word"), {
-      opacity: 0,
-      duration: 0.8,
-      y: 20,
-      stagger: 0.2,
-      delay: 0.9,
-    });
-  });
-
-  titleSectionTl
-    .from(
-      ".titleSection__main-container--image-container",
-      {
-        opacity: 0,
-        scale: 1.2,
-        ease: "Power3.inOut",
-        duration: 0.7,
-      },
-      "<0.1"
-    )
-    .from(
-      [".titleSection-text", ".titleSection-button"],
-      {
-        opacity: 0,
-        y: 20,
-        ease: "power3.inOut",
-      },
-      "<0.1"
-    )
-    .fromTo(
-      ".titleSection-clients",
-      { opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 0.7,
-        clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
-      },
-      "<0.3"
-    );
-
-  gsap.fromTo(
-    ".hero-section__hero-container--image img",
-    {
-      opacity: 0,
-    },
-    {
-      opacity: 1,
-      ease: "Power3.inOut",
-      duration: 1.2,
-      scrollTrigger: {
-        trigger: ".hero-section__hero-container--image img",
-        start: "top bottom-=20%",
-        end: "center top+=20%",
-        scrub: true,
-      },
-    }
-  );
-
-  heroSectionTitle.forEach((title) => {
-    gsap.from(title.querySelectorAll(".word"), {
-      opacity: 0,
-      y: 20,
-      stagger: 0.05,
-      scrollTrigger: {
-        trigger: title,
-        start: "top bottom-=20%",
-        end: "center top+=20%",
-        scrub: true,
-      },
-    });
-  });
-
-  heroSectionText.forEach((text) => {
-    gsap.fromTo(
-      text.querySelectorAll(".word"),
-      {
-        "will-change": "opacity",
-        opacity: 0.1,
-      },
-      {
-        ease: "none",
-        opacity: 1,
-        stagger: 0.05,
-        scrollTrigger: {
-          trigger: text,
-          start: "top bottom-=20%",
-          end: "center top+=20%",
-          scrub: true,
-        },
-      }
-    );
-  });
-
-  if (viewport > mobile) {
-    firstShowcaseTitle.forEach((text) => {
-      gsap.from(text.querySelectorAll(".word"), {
-        opacity: 0,
-        y: 20,
-        stagger: 0.05,
-        scrollTrigger: {
-          trigger: text,
-          start: "top bottom-=20%",
-          end: "center top+=20%",
-          scrub: true,
-        },
-      });
-    });
-
-    showcaseImages.forEach((image) => {
-      gsap.to(image, {
-        scale: 1.2,
-
-        scrollTrigger: {
-          trigger: image,
-          start: "top bottom-=20%",
-          end: "center top+=20%",
-          scrub: true,
-        },
-      });
-    });
-
-    gsap.from(".faqSection__container--questions-container", {
-      opacity: 0,
-      y: 20,
-      duration: 1,
-      scrollTrigger: {
-        trigger: ".faqSection__container--questions-container",
-        start: "top bottom-=20%",
-        end: "center top+=20%",
-        scrub: true,
-      },
-    });
-  }
-};
+//     gsap.from(".faqSection__container--questions-container", {
+//       opacity: 0,
+//       y: 20,
+//       duration: 1,
+//       scrollTrigger: {
+//         trigger: ".faqSection__container--questions-container",
+//         start: "top bottom-=20%",
+//         end: "center top+=20%",
+//         scrub: true,
+//       },
+//     });
+//   }
+// };
